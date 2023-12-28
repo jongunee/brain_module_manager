7bb8ac9b12e02718f37fc61b1634e4ae3335fdf9
jongunee <jejeoppa@naver.com>
2023-12-28 11:51:12 +0900

    save_with_bento optimized for other frameworks

--- app/bentoize.py
+++ app/bentoize.py
@@ -11,24 +11,26 @@ from . import utils
 def save_with_bento(file_path, framework, model_name, input_type, output_type):
     root, extension = os.path.splitext(file_path)
     print(extension)
+    result = None
+
     if framework == "sklearn":
         model = joblib.load(file_path)
         result = bentoml.sklearn.save_model(model_name, model)
-        tag = str(result.tag).split(":")[1]
-        utils.update_modeldata_db(model_name, framework, input_type, output_type, tag)
-        return result
     elif framework == "keras":
         model = keras.models.load_model(file_path)
-        return bentoml.keras.save_model(model_name, model)
+        result = bentoml.keras.save_model(model_name, model)
     elif framework == "tensorflow":
         if extension == ".h5":
             model = keras.models.load_model(file_path)
-            return bentoml.keras.save_model(model_name, model)
         else:
             model = tf.saved_model.load(file_path)
-            return bentoml.tensorflow.save_model(model_name, model)
+        result = bentoml.tensorflow.save_model(model_name, model)
     elif framework == "pytorch":
         model = torch.load(file_path)
-        return bentoml.pytorch.save_model(model_name, model)
+        result = bentoml.pytorch.save_model(model_name, model)
     else:
         return "Unsupported framework: {}".format(framework)
+    if result:
+        tag = str(result.tag).split(":")[1]
+        utils.update_modeldata_db(model_name, framework, input_type, output_type, tag)
+        return result
